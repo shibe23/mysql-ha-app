4e7e628526687273997a726d5874455579ef63d0
shibe23 <shibe23.info@gmail.com>
2025-05-25 20:45:22 +0900

    fix: masterの起動待ち処理を簡素化

--- mysql/replica1/setup/replica-init.sh
+++ mysql/replica1/setup/replica-init.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 
-echo "[INFO] Waiting for MySQL master to be ready..."
-
 # 60回 (最大約2分) 試行
-for i in {1..60}; do
-  if mysql -h mysql-master -uroot -prootpass -e "SELECT 1" >/dev/null 2>&1; then
-    echo "[INFO] MySQL master is available."
-    break
-  fi
-  echo "[INFO] Waiting ($i)..."
+echo "[INFO] Waiting for MySQL master to be ready..."
+until mysql -h mysql-master -uroot -prootpass -e "SELECT 1"; do
   sleep 2
 done
-
-if [ $i -eq 60 ]; then
-  echo "[ERROR] MySQL master did not become available in time."
-  exit 1
-fi
 
 echo "[INFO] Configuring replication..."
 mysql -uroot -prootpass -e "
